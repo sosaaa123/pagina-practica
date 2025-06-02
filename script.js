cb41a31946f9393401231981eebc1e51409fa673
sosaaa123 <lautasosita097@gmail.com>
2025-06-01 22:47:06 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+boton = document.querySelector(".boton")
+contenedor = document.querySelector(".contenedor")
+
+URL = "http://127.0.0.1:8000/"
+
+
+
+
+
+boton.addEventListener("click", ()=>{
+    contenedor.style.display = "flex"
+    contenedor.innerHTML = ""
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+         console.log(data)
+         data.forEach((i) => {
+            const nombreProducto = document.createElement("div")
+            const precioProducto = document.createElement("div")
+
+            nombreProducto.textContent = `Nombre: ${i.nombre}`
+            precioProducto.textContent = `Precio: $${i.precio}`
+
+            nombreProducto.classList.add("contenido-respuesta")
+            precioProducto.classList.add("contenido-respuesta")
+
+            const items = document.createElement("div")
+            items.classList.add("items")
+
+            items.appendChild(nombreProducto)
+            items.appendChild(precioProducto)
+
+
+            contenedor.appendChild(items)
+            
+         });
+
+    })
+    .catch(error => {
+        contenedor.style.display = "flex"
+        contenedor.innerHTML = ""
+        console.log(error)
+        const mensaje = document.createElement("div")
+        mensaje.textContent = `Parece que ha ocurrido el error: ${error}`
+        contenedor.appendChild(mensaje)
+    })
+
+
+
+
+
+})
